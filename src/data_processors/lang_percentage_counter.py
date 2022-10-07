f40f488a736c8d36f3feab65a0f61074ef94cf19
Viacheslav Kravchenko <vk@consulting.mk>
2022-10-07 18:15:43 +0300

    Refactoring

--- src/data_processors/lang_percentage_counter.py
+++ src/data_processors/lang_percentage_counter.py
@@ -1,13 +1,13 @@
 import pickle
 import time
-from datetime import timedelta, datetime
 
 import spacy
 from spacy.language import Language
 from spacy_langdetect import LanguageDetector
 
+from src.data_processors.counters import DefaultLanguagePercentageCounter
 from src.utils.file_name_utils import get_language_percentage_result_abs_file_name
-from src.utils.text_utils import remove_formatting, squash_sequential_message_from_same_person
+from src.utils.text_utils import remove_formatting
 
 
 def get_lang_detector(nlp, name):
@@ -17,24 +17,6 @@ def get_lang_detector(nlp, name):
 nlp = spacy.load("en_core_web_sm")
 Language.factory("language_detector", func=get_lang_detector)
 nlp.add_pipe('language_detector', last=True)
-
-
-def percentage(part, whole):
-    return 100 * float(part)/float(whole)
-
-
-def extract_lang_and_date(index_message_tuple):
-    index = index_message_tuple[0]
-    message = index_message_tuple[1]
-    dt = datetime.fromisoformat(message["date"])
-    week_start = dt - timedelta(days=dt.weekday())
-    result = {
-        'lang': message["detected_lang"],
-        'date': dt,
-        'beginning_of_week': week_start,
-        'beginning_of_week_str': week_start.strftime("%d/%m/%Y"),
-    }
-    return result
 
 
 def count_lang_percentage_and_save_to_file(data, file_name, user_stop_list):
@@ -54,10 +36,13 @@ def count_lang_percentage(data, user_stop_list=[]):
 
 
 def count_language_percentages(messages):
-    number_of_messages_by_weeks = count_number_of_messages_by_time_period(messages)
-    lang_percentage_by_weeks = count_percentages(number_of_messages_by_weeks)
-    result = convert_to_result_dto(lang_percentage_by_weeks)
-    return result
+    use_weighted_counter = False
+    if use_weighted_counter:
+        # split messages by weeks
+        # in each week
+        return {}
+    else:
+        return DefaultLanguagePercentageCounter().count_language_percentages(messages)
 
 
 def clean_data(data, user_stop_list):
@@ -92,45 +77,6 @@ def filter_users_by_stop_list(messages, user_stop_list):
 def user_is_not_in_stop_list(message, user_stop_list):
     user_is_in_stop_list = message['from_id'] in user_stop_list
     return not user_is_in_stop_list
-
-
-def count_number_of_messages_by_time_period(messages):
-    minimized_messages = list(map(extract_lang_and_date, enumerate(messages)))
-    number_of_messages_by_weeks = {}
-    for m in minimized_messages:
-        if m['beginning_of_week_str'] not in number_of_messages_by_weeks:
-            number_of_messages_by_weeks[m['beginning_of_week_str']] = {'uk': 0, 'ru': 0}
-        number_of_messages_by_weeks[m['beginning_of_week_str']][m['lang']] = \
-            number_of_messages_by_weeks[m['beginning_of_week_str']][m['lang']] + 1
-    return number_of_messages_by_weeks
-
-
-def count_percentages(number_of_messages_by_weeks):
-    messages_percentage = {}
-    for week_start, messages_counts in number_of_messages_by_weeks.items():
-        if week_start not in messages_percentage:
-            messages_percentage[week_start] = {'uk': 0.0, 'ru': 0.0}
-        total_number_of_messages_per_week = messages_counts['uk'] + messages_counts['ru']
-        uk_percentage = percentage(messages_counts['uk'], total_number_of_messages_per_week)
-        ru_percentage = percentage(messages_counts['ru'], total_number_of_messages_per_week)
-        messages_percentage[week_start]['uk'] = uk_percentage
-        messages_percentage[week_start]['ru'] = ru_percentage
-        print(
-            f"Week: {week_start}. Total number of messages: {total_number_of_messages_per_week}. Uk: {messages_counts['uk']}. Ru: {messages_counts['ru']}. Uk percentage: {uk_percentage}. Ru percentage: {ru_percentage}")
-    return messages_percentage
-
-
-def convert_to_result_dto(messages_percentage):
-    result = {
-        'week_start': [],
-        'uk_percentage': [],
-        'ru_percentage': []
-    }
-    for week_start, language_percentages in messages_percentage.items():
-        result['week_start'].append(week_start)
-        result['uk_percentage'].append(language_percentages['uk'])
-        result['ru_percentage'].append(language_percentages['ru'])
-    return result
 
 
 def detect_language(mapped_messages):
